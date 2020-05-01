bc661030f348fa0568f71b87136970d658a2d485
DosImpact <ypd03008@gmail.com>
2020-05-01 17:08:28 +0900

    hello3

--- src/api/hello.js
+++ src/api/hello.js
@@ -3,6 +3,8 @@ import naver from "../crwaling/naver";
 export default {
   Query: {
     hello: () => "HELLO GUI Crawling world",
+    hello2: () => "HELLO 2",
+    hello3: (_, args) => `HELLO 3 ${args.id}`,
     naverDust: async () => naver(),
   },
 };
