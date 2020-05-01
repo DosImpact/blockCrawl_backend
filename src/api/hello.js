ab78000a9b74af7f4e62948f0fe91463661b0ed7
DosImpact <ypd03008@gmail.com>
2020-05-01 17:19:25 +0900

    useLazyQuery

--- src/api/hello.js
+++ src/api/hello.js
@@ -5,6 +5,10 @@ export default {
     hello: () => "HELLO GUI Crawling world",
     hello2: () => "HELLO 2",
     hello3: (_, args) => `HELLO 3 ${args.id}`,
+    hello4: (_, args) =>
+      ` HELLO 4 : id : ${args.id} yourname: ${
+        args.name
+      } your infos :  ${JSON.stringify(args.info)}`,
     naverDust: async () => naver(),
   },
 };
