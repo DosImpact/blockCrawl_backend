3c1cdec225d09fe80713a121d9fee6a5fdf6549b
DosImpact <ypd03008@gmail.com>
2020-07-15 15:55:16 +0900

    mongodb connect

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import express from "express";
+import mongoose from "mongoose";
 
 import { brs, error, init, reLaunch } from "./crwaling/browser";
 
@@ -13,6 +14,16 @@ import path from "path";
  ========================mongo DB
  */
 
+const connect = mongoose
+  .connect(process.env.DB_URI, {
+    dbName: "blockcrawl",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("✔ MongoDB connected..."))
+  .catch((e) => console.error(`❌ MongoDB disconnected`, e));
 /*
  ========================Server setting &&  MiddleWare
  */
@@ -29,7 +40,15 @@ server.express.use(bodyParser.json());
 server.express.use(bodyParser.urlencoded({ extended: true }));
 server.express.use(cookieParser());
 
-server.express.use("/api/test", require("./router/test"));
+server.express.use("/api/test", require("./routes/test"));
+server.express.use("/api/users", require("./routes/users"));
+server.express.use((req, res, next) => {
+  res.status(404).send("NOT FOUND 😡");
+});
+server.express.use((err, req, res, next) => {
+  console.error("❌ Server Error", err);
+  res.end("❌ Server Error", err);
+});
 
 server.express.use(express.static("public"));
 server.express.get("/download/*", (req, res) => {
